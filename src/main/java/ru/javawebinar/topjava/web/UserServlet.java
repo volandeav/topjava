b7a271bb3307a28677bff1e2ec545a6fd4fb39ac
voland <voland>
2020-02-27 23:28:28 +0100

    clear, add log

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.web;
+
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    private static final Logger log = getLogger(UserServlet.class);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/users.jsp").forward(request,response);
+        log.debug("UserServletRedirect");
+    }
+}
